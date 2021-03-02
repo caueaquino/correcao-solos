2a7e4d3d02cd78d8bf2d6630de566eadaba1b84d
CaueAN <cauenogueira@alunos.utfpr.edu.br>
2021-03-01 22:15:17 -0300

    add regras calculo valores potassio

--- src/main/java/tabelaTeores/Fosforo.java
+++ src/main/java/tabelaTeores/Fosforo.java
@@ -2,28 +2,57 @@ package tabelaTeores;
 
 public class Fosforo {
 
-    private double ValorIdeal = 9.0;
-    private int Valor;
-    private int ValorCorrigido;
+    private double Valor;
+    private double ValorAposCorrecao;
+    private double ValorIdeal;
+    private double ValorParaAtingir;
 
-    public Fosforo(int valor) {
-        Valor = valor;
+    public Fosforo() { }
+
+    public double getValor() {
+        return Valor;
+    }
+
+    public double getValorAposCorrecao() {
+        return ValorAposCorrecao;
     }
 
     public double getValorIdeal() {
         return ValorIdeal;
     }
 
-    public int getValor() {
-        return Valor;
+    public void setValor(double valor) {
+        Valor = valor;
     }
 
-    public int getValorCorrigido() {
-        return ValorCorrigido;
+    public void setValorParaAtingir(double valorParaAtingir) {
+        ValorParaAtingir = valorParaAtingir;
     }
 
-    public void CalculaCorrecaoFosforo() {
+    public void calculaValorIdeal(int texturaSolo) {
+        switch (texturaSolo) {
 
+            case 1:
+                ValorIdeal = 9;
+                break;
+
+            case 2:
+                ValorIdeal = 12;
+                break;
+
+            default:
+                ValorIdeal = 0;
+                break;
+
+        }
+    }
+
+    public void calculaValorAposCorrecao() {
+        if (ValorParaAtingir > 0.01) {
+            ValorAposCorrecao = ValorParaAtingir;
+            return;
+        }
+        ValorAposCorrecao = 0;
     }
 
 }
